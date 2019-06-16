5f654752af06928aea908a84c27ae1f5618cf523
radko.lyutskanov <rlyutskanov@gmail.com>
2019-06-16 13:31:22 +0300

    adding more discount service intergration tests

--- src/test/java/com/retailstore/discounts/service/DiscountServiceIntegrationTest.java
+++ src/test/java/com/retailstore/discounts/service/DiscountServiceIntegrationTest.java
@@ -25,38 +25,66 @@ public class DiscountServiceIntegrationTest {
 
 	@Test
 	public void applyOnlyPercentageDiscount() {
-		DiscountedBillDto discountedBillDto = discountService.applyDiscount(TestHelper.createPrincipal(TestHelper.EMPLOYEE_ACC, TestHelper.COMMON_PASSWORD, null), TestHelper.createBillDto(BigDecimal.valueOf(50.50), false));
+		DiscountedBillDto discountedBillDto = discountService.applyDiscount(
+				TestHelper.createPrincipal(TestHelper.EMPLOYEE_ACC, TestHelper.COMMON_PASSWORD, null),
+				TestHelper.createBillDto(BigDecimal.valueOf(50.50), false));
 		Assert.assertEquals(discountedBillDto.getBill(), BigDecimal.valueOf(50.50));
 		Assert.assertEquals(discountedBillDto.getDiscountedBill(), BigDecimal.valueOf(35.35)); // 15.15 discount
 	}
 
 	@Test
 	public void applyPercentageAndAmountDiscount() {
-		DiscountedBillDto discountedBillDto = discountService.applyDiscount(TestHelper.createPrincipal(TestHelper.EMPLOYEE_ACC, TestHelper.COMMON_PASSWORD, null), TestHelper.createBillDto(BigDecimal.valueOf(150), false));
+		DiscountedBillDto discountedBillDto = discountService.applyDiscount(
+				TestHelper.createPrincipal(TestHelper.EMPLOYEE_ACC, TestHelper.COMMON_PASSWORD, null),
+				TestHelper.createBillDto(BigDecimal.valueOf(150), false));
 		Assert.assertEquals(discountedBillDto.getBill(), BigDecimal.valueOf(150));
 		Assert.assertEquals(discountedBillDto.getDiscountedBill(), BigDecimal.valueOf(100)); // 45 + 5 discount
+	}
+
+	@Test
+	public void applyPercentageAndAmountDiscountAffiliate() {
+		DiscountedBillDto discountedBillDto = discountService.applyDiscount(
+				TestHelper.createPrincipal(TestHelper.AFFILIATE_ACC, TestHelper.COMMON_PASSWORD, null),
+				TestHelper.createBillDto(BigDecimal.valueOf(150), false));
+		Assert.assertEquals(discountedBillDto.getBill(), BigDecimal.valueOf(150));
+		Assert.assertEquals(discountedBillDto.getDiscountedBill(), BigDecimal.valueOf(130)); // 15 + 5 discount
 	}
 
 
 	@Test
 	public void applyOnlyAmountDiscount() {
-		DiscountedBillDto discountedBillDto = discountService.applyDiscount(TestHelper.createPrincipal(TestHelper.CUSTOMER_ACC, TestHelper.COMMON_PASSWORD, null), TestHelper.createBillDto(BigDecimal.valueOf(150), false));
+		DiscountedBillDto discountedBillDto = discountService.applyDiscount(
+				TestHelper.createPrincipal(TestHelper.CUSTOMER_ACC, TestHelper.COMMON_PASSWORD, null),
+				TestHelper.createBillDto(BigDecimal.valueOf(150), false));
 		Assert.assertEquals(discountedBillDto.getBill(), BigDecimal.valueOf(150));
 		Assert.assertEquals(discountedBillDto.getDiscountedBill(), BigDecimal.valueOf(145)); // 5 discount
 	}
 
 	@Test
 	public void applyLongTermDiscount() {
-		DiscountedBillDto discountedBillDto = discountService.applyDiscount(TestHelper.createPrincipal(TestHelper.CUSTOMER_LONGTERM_ACC, TestHelper.COMMON_PASSWORD, null), TestHelper.createBillDto(BigDecimal.valueOf(150), false));
+		DiscountedBillDto discountedBillDto = discountService.applyDiscount(
+				TestHelper.createPrincipal(TestHelper.CUSTOMER_LONGTERM_ACC, TestHelper.COMMON_PASSWORD, null),
+				TestHelper.createBillDto(BigDecimal.valueOf(150), false));
 		Assert.assertEquals(discountedBillDto.getBill(), BigDecimal.valueOf(150));
 		Assert.assertEquals(discountedBillDto.getDiscountedBill(), BigDecimal.valueOf(137.5)); // 5% + 5$ discount
 	}
 
 	@Test
 	public void applyOnlyLongTermDiscount() {
-		DiscountedBillDto discountedBillDto = discountService.applyDiscount(TestHelper.createPrincipal(TestHelper.CUSTOMER_LONGTERM_ACC, TestHelper.COMMON_PASSWORD, null), TestHelper.createBillDto(BigDecimal.valueOf(50), false));
+		DiscountedBillDto discountedBillDto = discountService.applyDiscount(TestHelper.createPrincipal(
+				TestHelper.CUSTOMER_LONGTERM_ACC, TestHelper.COMMON_PASSWORD, null),
+				TestHelper.createBillDto(BigDecimal.valueOf(50), false));
 		Assert.assertEquals(discountedBillDto.getBill(), BigDecimal.valueOf(50));
 		Assert.assertEquals(discountedBillDto.getDiscountedBill(), BigDecimal.valueOf(47.5)); // 5% discount
+	}
+
+	@Test
+	public void applyOnlyEmployeeDiscount() {
+		DiscountedBillDto discountedBillDto = discountService.applyDiscount(TestHelper.createPrincipal(
+				TestHelper.EMPLOYEE_LONGTERM_ACC, TestHelper.COMMON_PASSWORD, null),
+				TestHelper.createBillDto(BigDecimal.valueOf(50), false));
+		Assert.assertEquals(discountedBillDto.getBill(), BigDecimal.valueOf(50));
+		Assert.assertEquals(discountedBillDto.getDiscountedBill(), BigDecimal.valueOf(35)); // 30% discount
 	}
 
 }
